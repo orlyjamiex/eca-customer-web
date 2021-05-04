deb3422bea57fa0a7f9669a0402861fb88cdf469
orlyjamiex <64273185+orlyjamiex@users.noreply.github.com>
2021-05-04 23:11:15 +1000

    Add files via upload

---
+++ server.js
@@ -0,0 +1,5 @@
+var main = require('./Backend/main');
+//Use for dev-fctgl-au.flitech.net and preprod AWS
+//aws_access = AKIAXYZDQCEN2P7FDNPJ
+//aws_secret = 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
+main.startServer(5050);
